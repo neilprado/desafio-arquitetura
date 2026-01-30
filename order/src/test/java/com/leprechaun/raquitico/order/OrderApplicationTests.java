0461b5185b54f65cb672283e69e2bfcfb5193a4e
neilprado <neiljohn17@gmail.com>
2026-01-30 18:50:12 -0300

    Adding spring boot base project

---
+++ order/src/test/java/com/leprechaun/raquitico/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leprechaun.raquitico.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
